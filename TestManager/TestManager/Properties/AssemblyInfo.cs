1e62949fb6592a8c5a94e4f391df3b2053ac0c34
Sudhir <sudhir.kr84@gmail.com>
2017-11-17 20:36:24 +0530

    initial checkins- creating UI layout

--- TestManager/TestManager/Properties/AssemblyInfo.cs
+++ TestManager/TestManager/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestManager")]
+[assembly: AssemblyTitle("Sudhir.TestManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Sudhir Srivastava")]
 [assembly: AssemblyProduct("TestManager")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Sudhir Srivastava 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
